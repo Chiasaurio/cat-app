9e368a4afa8aa2d41f875bcbec132d5d1ddbfdb0
Jose Andres Chia Lerzundy <andreschiia1@gmail.com>
2024-11-24 20:42:39 -0500

    launch icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'modules/cats/_lib.dart';
 
-void main() {
+void main() async {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // SplashScreenService.preserve(widgetsBinding);
+  // Add a delay
+  // await Future.delayed(Duration(seconds: 10)); // Adjust the duration as needed
+
   runApp(const MyApp());
 }
 
@@ -19,5 +24,26 @@ class MyApp extends StatelessWidget {
       ),
       home: const MainPageCatsModule(),
     );
+  }
+}
+
+class SplashScreenService {
+  static late WidgetsBinding _widgetsBinding;
+
+  /// Splash screen is being diisplayed
+  static bool _splashScreenIsOn = true;
+
+  /// Maintains the splash screen until the method remove() is called
+  static void preserve(WidgetsBinding widgetsBinding) {
+    _widgetsBinding = widgetsBinding;
+    _widgetsBinding.deferFirstFrame();
+  }
+
+  /// Removes splashScreen. It only works if the splashScreen is being displayed
+  static void remove() {
+    if (_splashScreenIsOn) {
+      _widgetsBinding.allowFirstFrame();
+      _splashScreenIsOn = false;
+    }
   }
 }
